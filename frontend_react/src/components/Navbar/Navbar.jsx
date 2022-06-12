5ecdf26efba3d610d52ac18847ed8e25c31a89bf
Viraj-Mahalunkar <mahalunkar.viraj@gmail.com>
2022-06-12 14:32:25 +0530

    first commit to the practice portfolio tutorial from YT

---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { images } from '../../constants';
+
+import './Navbar.scss';
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src={'images.logo'}></img>
+        </div>
+    </nav>
+  )
+}
+
+
+export default Navbar
